3cadf5f0aae8100a4a8691542d43798bfc72d0e5
Lena Niku <niku.elena@gmail.com>
2022-11-02 20:45:46 +0300

    lamda

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,36 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MyComporator myComporator = new MyComporator();
-
-        Person person1 = new Person("Andrey", "Petrov", 34);
-        Person person2 = new Person("Arseniy", "Vyskov-Tupson", 21);
+        //MyComporator myComporator = new MyComporator();
 
         List<Person> persons = new ArrayList<>();
-        persons.add(person1);
-        persons.add(person2);
-        Collections.sort(persons, myComporator);
+        persons.add(new Person("Andrey", "Petrov", 34));
+        persons.add(new Person("Arseniy", "Vyskov-Tupson", 21));
+        persons.add(new Person("Ivan", "Ivanov", 15));
+        persons.add(new Person("Alex", "Alex", 16));
+
+        //System.out.println(persons);
+
+        persons.sort(
+                (one, two) -> {
+                    int length1 = one.getSurname().split("-").length;
+                    int length2 = two.getSurname().split("-").length;
+                    if (length1 != length2) {
+                        return Integer.compare(length1, length2);
+                    }
+
+                    return Integer.compare(one.getAge(), two.getAge()); }
+        );
+
         System.out.println(persons.get(persons.toArray().length - 1));
+
+        persons.removeIf(
+                (person -> {
+                    return person.getAge() < 18;
+                })
+        );
+
+        //System.out.println(persons);
+
     }
 }
